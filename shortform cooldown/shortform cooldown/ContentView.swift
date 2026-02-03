84d762edd65bfe17295f0f0abcc2409c28d05515
almastelek <almasatelek@gmail.com>
2026-02-02 21:25:21 -0500

    initial ios project scaffold

---
+++ shortform cooldown/shortform cooldown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shortform cooldown
+//
+//  Created by Almas Telek on 2/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shortform cooldown/shortform cooldown/shortform_cooldownApp.swift
@@ -0,0 +1,17 @@
+//
+//  shortform_cooldownApp.swift
+//  shortform cooldown
+//
+//  Created by Almas Telek on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct shortform_cooldownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
